250a28943a3642c7d28069965d053e4a6534b32c
amelia <ameliawalcek@gmail.com>
2021-02-06 13:31:58 +0200

    useref

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react'
+import { useInputValue } from './hooks'
 
 function App() {
+
+  const inputRef = useRef(null)
+  const prevName = useRef('')
+
+  const [name, setName] = useState('')
+  // const [name, setName] = useInputValue('')
+
+  const focusInput = () => {
+    inputRef.current.focus()
+  }
+
+  useEffect(() => {
+    prevName.current = name
+  }, [name])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        ref={inputRef}
+        value={name}
+        onChange={({ target }) => setName(target.value)}
+      />
+      <button onClick={focusInput}>Focus</button>
+      <div>My name is {name} it used to be {prevName.current}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
